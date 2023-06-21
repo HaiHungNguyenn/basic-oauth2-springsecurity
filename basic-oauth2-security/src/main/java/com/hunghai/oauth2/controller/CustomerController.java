2e7397083b092a646f9d291a480d70cfbeceef01
HaiHungNguyenn <hainhse173100@fpt.edu.vn>
2023-06-21 23:09:29 +0700

    adding authorization for user and admin role

--- basic-oauth2-security/src/main/java/com/hunghai/oauth2/controller/CustomerController.java
+++ basic-oauth2-security/src/main/java/com/hunghai/oauth2/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.hunghai.oauth2.controller;
 
 import com.hunghai.oauth2.model.Customer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,11 @@ public class CustomerController {
         List<Customer> customers = this.customers.stream().filter(customer -> customer.getId().equals(id)).toList();
         return ResponseEntity.ok(customers.get(0));
 
+    }
+    @GetMapping("/customer/all")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    public ResponseEntity<List<Customer>> getCustomerList(){
+        List<Customer> customers = this.customers;
+        return ResponseEntity.ok(customers);
     }
 }
